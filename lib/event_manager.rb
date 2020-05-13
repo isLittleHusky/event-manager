57d51753cfa10396570ccb2b4e38f6d2fe98f001
Dylan <jddekock2001@gmail.com>
2020-05-13 13:37:27 +0200

    use csv lib to parse event_attendees.csv

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,18 @@
+require "csv"
 puts "Event Manager initialized"
 
-lines = File.readlines "event_attendees.csv"
-for lines.each do |line|
-	
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+
+  if zipcode == nil
+    zipcode = "00000"
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  elsif zipcode.length < 5
+    zipcode.rjust(5 - zipcode.length, "0")
+  end
+
+  puts "#{name} #{zipcode}"
+end
